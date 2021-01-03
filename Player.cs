58dba38dc0dbaa9bb0ba928af6be93badea1fc39
carlos pizano <carlos.pizano@gmail.com>
2021-01-02 17:24:03 -0800

    Move Player state to its own file.

--- Player.cs
+++ Player.cs
@@ -1,61 +1,8 @@
-﻿using System;
-using System.Collections;
-using System.Drawing;
+﻿using System.Drawing;
 using System.Numerics;
-using System.Text;
 
 namespace TFIServer
 {
-    public enum TransitState
-    {
-        Frozen,     // Cannot move at all.
-        Ground,     // Can move in the plane anywhere
-        Threshold,  // Entering or exiting a special zone
-        Stairs,     // Can move up and down.
-        ClosedArea, // Restricted to an area.
-    };
-
-    public readonly struct PlayerState
-    {
-        public readonly Vector2 position;
-        public readonly int z_level;
-        public readonly int health;
-        public readonly TransitState transit_state;
-
-        public PlayerState(Vector2 _pos, int _z_level, int _health, TransitState _state)
-        {
-            position = _pos;
-            z_level = _z_level;
-            health = _health;
-            transit_state = _state;
-        }
-
-        public PlayerState(in PlayerState o)
-        {
-            position = o.position;
-            z_level = o.z_level;
-            health = o.health;
-            transit_state = o.transit_state;
-        }
-
-        public PlayerState(in PlayerState o, Vector2 _pos) : this(o)
-        {
-            position = _pos;
-        }
-
-        public PlayerState(in PlayerState o, int _z_level, Vector2 _pos) : this(o)
-        {
-            position = _pos;
-            z_level = _z_level;
-        }
-
-        public PlayerState(in PlayerState o, TransitState ts, Vector2 _pos) : this(o)
-        {
-            transit_state = ts;
-            position = _pos;
-        }
-    }
-
     // This object methods should only be called from the gamethread.
     class Player
     {
